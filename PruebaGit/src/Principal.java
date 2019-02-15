77172a18ccbf40f8d4f820e7f8d163819268092e
albadelaconcha <alba.delaconcha@yahoo.es>
2019-02-15 13:51:24 +0100

    Cambio 2

--- PruebaGit/src/Principal.java
+++ PruebaGit/src/Principal.java
@@ -2,6 +2,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		int numero = 5;
+		int a = 2;
 		for (int i=1; i<=10; i++) {
 			System.out.println
 (numero + " x " + i + " = " + (numero*i));
